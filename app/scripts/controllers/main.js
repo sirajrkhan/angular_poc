f87c612ace1a2a53bd33004e2faa04aa44449800
sirajrkhan <sirajrkhan@live.in>
2014-10-16 13:57:43 +0000

    updated todo

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,11 +7,26 @@
  * # MainCtrl
  * Controller of the angularPocApp
  */
+
 angular.module('angularPocApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+    .controller('MainCtrl',function ($scope){
+    $scope.cars = [
+        'Audi',
+        'Lexus',
+        'Jaguar',
+        'Benz',
+        'BMW'
     ];
-  });
+    $scope.todos = [
+        'To Do Item No. 1',
+        'To Do Item No. 2',
+        'To Do Item No. 3',
+        'To Do Item No. 4'
+    ];
+    $scope.addTodo = function(){
+      //$scope.todos.push($scope.todo);
+      //$scope.todo='';
+      console.log('function addTodo called........................');
+      $scope.todos.push($scope.todo);
+    };
+});
